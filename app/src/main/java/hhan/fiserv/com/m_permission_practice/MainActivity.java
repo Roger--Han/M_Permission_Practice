4b7e8678a2b86503f72e6c6a258876f070b1b937
Roger Han <hong.han@fiserv.com>
2017-04-20 15:37:02 +1200

    Initial commit

---
+++ app/src/main/java/hhan/fiserv/com/m_permission_practice/MainActivity.java
@@ -0,0 +1,22 @@
+package hhan.fiserv.com.m_permission_practice;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int EXTERNAL_STORAGE_PERMISSION_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
+    private boolean sentToSettings = false;
+    private SharedPreferences permissionStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
